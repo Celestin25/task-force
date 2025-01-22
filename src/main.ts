79b5a26b9dd00e3cd5847bf6949974913376aac0
Celestin25 <celestinhakorimana25@gmail.com>
2025-01-22 18:00:42 +0200

    cors error resolved

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,23 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = [
+    'http://example1.com',
+    'http://example2.com',
+    'http://localhost:3001',
+  ];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
